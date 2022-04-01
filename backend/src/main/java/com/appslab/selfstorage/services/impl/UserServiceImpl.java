ad22f41219cc1a7f0f2c39d8c390b947a42a2f16
vladimirtischler <tischler.vladimir@gmail.com>
2022-04-01 19:21:39 +0200

    default profile picture after registration

--- backend/src/main/java/com/appslab/selfstorage/services/impl/UserServiceImpl.java
+++ backend/src/main/java/com/appslab/selfstorage/services/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.appslab.selfstorage.services.impl;
 
+import com.appslab.selfstorage.config.DocumentStorageProperty;
 import com.appslab.selfstorage.dto.LocalUser;
 import com.appslab.selfstorage.dto.SignUpRequest;
 import com.appslab.selfstorage.dto.SocialProvider;
@@ -12,6 +13,7 @@ import com.appslab.selfstorage.security.oauth2.user.OAuth2UserInfoFactory;
 import com.appslab.selfstorage.services.UserService;
 
 import com.appslab.selfstorage.util.GeneralUtils;
+import lombok.SneakyThrows;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,6 +23,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -31,13 +40,19 @@ public class UserServiceImpl implements UserService {
     private RoleRepository roleRepository;
     private FileRepositoryDB fileRepositoryDB;
     private CategoryRepository categoryRepository;
+    private Path docStorageLocation;
 
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository, FileRepositoryDB fileRepositoryDB, CategoryRepository categoryRepository) {
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository, FileRepositoryDB fileRepositoryDB, CategoryRepository categoryRepository, DocumentStorageProperty documentStorageProperty) throws IOException {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.roleRepository = roleRepository;
         this.fileRepositoryDB = fileRepositoryDB;
         this.categoryRepository = categoryRepository;
+        this.docStorageLocation = Paths.get(documentStorageProperty.getUploadDirectory()).toAbsolutePath().normalize();
+        Files.createDirectories(this.docStorageLocation);
+
+
     }
 
     @Override
@@ -51,9 +66,10 @@ public class UserServiceImpl implements UserService {
         return "failed";
     }
 
+    @SneakyThrows
     @Override
     @Transactional(value = "transactionManager")
-    public User registerNewUser(final SignUpRequest signUpRequest) throws UserAlreadyExistAuthenticationException {
+    public User registerNewUser(final SignUpRequest signUpRequest) throws UserAlreadyExistAuthenticationException{
         if (signUpRequest.getUserID() != null && userRepository.existsById(signUpRequest.getUserID())) {
             throw new UserAlreadyExistAuthenticationException("User with User id " + signUpRequest.getUserID() + " already exist");
         } else if (userRepository.existsByEmail(signUpRequest.getEmail())) {
@@ -64,6 +80,7 @@ public class UserServiceImpl implements UserService {
         user.setCreatedDate(now);
         user.setModifiedDate(now);
         user.setSpaceSize(2000000000L);
+        setDefaultProfilePicture(user);
 
         userRepository.save(user);
         userRepository.flush();
@@ -191,5 +208,30 @@ public class UserServiceImpl implements UserService {
         String username = userDetails.getUsername();
         User user = userRepository.findByEmail(username);
         return user.getId();
+    }
+
+    public File setDefaultProfilePicture(User user) throws Exception{
+        java.io.File defaultPicture = new java.io.File("frontend/src/assets/images/default_profile_picture.png");
+        File picture = new File();
+        picture.setName("default_profile_picture.png");
+        picture.setMimeType("image/png");
+        picture.setDate();
+        picture.setOwnerId(user.getId());
+        picture.setUuid();
+
+        java.io.File file = docStorageLocation.resolve(picture.getUuid().toString()).toFile();
+        file.createNewFile();
+        FileOutputStream outputStream = new FileOutputStream(file);
+        InputStream inputStream = new FileInputStream(defaultPicture);
+        inputStream.transferTo(outputStream);
+        outputStream.close();
+
+        user.setProfilePicture(picture);
+        picture.setOwnerProfilePicture(user);
+
+        fileRepositoryDB.save(picture);
+        userRepository.save(user);
+
+        return picture;
     }
 }
